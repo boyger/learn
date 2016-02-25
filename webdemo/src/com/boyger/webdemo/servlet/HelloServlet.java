4cfbc35e90b161b34d58f3c37228580a26917de2
wangyongbo <boyger@163.com>
2016-02-25 17:30:34 +0800

    git test

--- webdemo/src/com/boyger/webdemo/servlet/HelloServlet.java
+++ webdemo/src/com/boyger/webdemo/servlet/HelloServlet.java
@@ -8,19 +8,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("hello")
+@WebServlet(value = "/hello", loadOnStartup = 1)
 public class HelloServlet extends HttpServlet
 {
 	private static final long	serialVersionUID	= 1L;
 
-	public HelloServlet()
-	{}
+	@Override
+	public void init() throws ServletException
+	{
+		log("init invoked");
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-	{}
+	{
+		resp.getWriter().write("Hello! " + req.getRemoteHost());
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{}
+
+	@Override
+	public void destroy()
+	{
+		log("destory invoked");
+	}
 }
