108f853ed46f799926dc07b807f1bfc84082fd0c
wangyongbo <boyger@163.com>
2016-02-26 16:30:01 +0800

    webserver

---
+++ webserver/src/com/boyger/webserver/servlet/TestServlet.java
@@ -0,0 +1,23 @@
+package com.boyger.webserver.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		resp.getWriter().printf("%s -> %s:hello there.", req.getRemoteAddr(),req.getLocalAddr());
+		resp.getWriter().flush();
+	}
+
+}
